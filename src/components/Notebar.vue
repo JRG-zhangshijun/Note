586463a54878624b333cfeca208116a0a21d5a6e
JRG-zhangshijun <462895506@qq.com>
2017-09-22 23:11:11 +0800

    添加创建时间有问题

---
+++ src/components/Notebar.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="notebar">
+    <a href="#" class="iconfont" @click="newNote">&#xe669;</a>
+    <a href="#" class="iconfont" @click="toggleFavorite" :class="{activeIcon: selected}">&#xe601;</a>
+    <a href="#" class="iconfont" @click="deleteNote">&#xe50f;</a>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import '../assets/iconfont.css'
+
+  export default {
+    name: 'Notetbar',
+    computed: {
+      selected() {
+        return this.$store.state.activeNote.favorite
+      }
+    },
+    methods: {
+      newNote() {
+        this.$store.commit('NewNote')
+      },
+      deleteNote() {
+        this.$store.commit('DeleteNote')
+      },
+      toggleFavorite() {
+        this.$store.commit('ToggleFavorite')
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  #notebar {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 80px;
+    background: #30414d;
+    > .iconfont {
+      display: block;
+      margin-top: 36px;
+      font-size: 2.2em;
+      color: #686b6e;
+      text-decoration: none;
+    }
+    > .activeIcon {
+      color: #F7AE4F;
+    }
+  }
+</style>
